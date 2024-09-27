dbbab4f7eafa3c1b20026c914aacf8bb3d3aada2
morganF0 <m.fletcher.34@student.scu.edu.au>
2024-09-27 08:05:04 +0800

    Begin new fundraiser submission

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -25,5 +25,56 @@ function displayFundraisers(fundraisers) {
     });
 }
 
+// Handle new fundraiser using form submission
+async function handleNewFundraiserSubmit() {
+    // Get values from form
+    const organiser = document.getElementById('new-organiser').value;
+    const caption = document.getElementById('new-caption').value;
+    const targetFunding = document.getElementById('new-target-funding').value;
+    const city = document.getElementById('new-city').value;
+    const category = document.getElementById('new-category').value;
+    const imgUrl = document.getElementById('new-img-url').value;
+
+    // simple validation, might need to improve
+    if (organiser === '' || caption === '' || targetFunding === '' || city === '' || category === '') {
+        alert('Please fill in all required fields.')
+        return;
+    }
+
+
+    // Prepare data to send in request body
+    const newFundraiserData = {
+        ORGANISER: organiser,
+        CAPTION: caption,
+        TARGET_FUNDING: targetFunding,
+        CURRENT_FUNDING: 0, // bold assumption here
+        CITY: city,
+        ACTIVE: true, // another assumption
+        CATEGORY_ID: getCategoryID(category),
+        IMG_URL: imgUrl
+    };
+
+    try {
+        const response = await fetch(`${apiUrl}/add-fundraiser`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFundraiserData)
+        });
+
+        if (response.ok) {
+            alert('New fundraiser added successfully!');
+        } else {
+            console.error('Error adding fundraiser:', response.statusText);
+            alert('Error adding fundraiser. Please try again later.');
+        }
+    } catch (error) {
+        console.error('Error adding fundraiser:', error);
+        alert('Error adding fundraiser. Please try again later.');
+    }
+}
+
+
 // Fetch and display fundraisers when the page loads
 window.onload = fetchAllFundraisers;
